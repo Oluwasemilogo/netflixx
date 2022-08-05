e55a5401c62e5c8713acde75957286078de2dfac
Semilogo Bamidele <emmanbamidele471@gmail.com>
2022-08-05 14:15:29 +0100

    Frontend and Api
    Completed the frontend and Fetched api from a movie api (TMDB)

---
+++ src/Row.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import './Row.css';
+import axios from './axios';
+
+
+function Row({title, fetchUrl, isLargeRow= false }) {
+    const [movies, setMovies] = useState([]);
+     const base_url = "https://image.tmdb.org/t/p/original/"
+    useEffect(() =>{
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            console.log(request);
+             setMovies(request.data.results);
+           return request;
+
+        }
+      fetchData();
+    }, [fetchUrl]);
+   console.log(movies);
+  return (
+    <div className='row'>
+        <h2> {title} </h2>
+        <div className="row_posters">
+                {movies.map(movie =>(
+                <img className={`row_poster &{isLargeRow && 'row_posterLarge' }`}
+                key={movie.id}
+                src={`${base_url}${
+                isLargeRow ? movie.poster_path :movie.backdrop_path
+                }`} 
+                alt= {movie.name} />
+            ))}
+            <h4>{movies?.title || movies?.name || movies?.original_name}</h4>
+    </div>
+        </div>
+           
+  )
+}
+ 
+export default Row
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDR7UnXwheVkcYIpOzFs5HbqY_OU2urQ0",
+    authDomain: "netflixx-84100.firebaseapp.com",
+    projectId: "netflixx-84100",
+    storageBucket: "netflixx-84100.appspot.com",
+    messagingSenderId: "105151578578",
+    appId: "1:105151578578:web:2e16fc729483792c26c80b",
+    measurementId: "G-BYFN7ZPLVX"
+  };
